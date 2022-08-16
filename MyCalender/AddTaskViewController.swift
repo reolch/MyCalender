ca7349ee338649ff756cae681b6ca916f471cae6
Reolch <>
2022-08-16 14:33:11 +0900

    #3 リファクタリング 処理の共通化などを実施した。

--- MyCalender/AddTaskViewController.swift
+++ MyCalender/AddTaskViewController.swift
@@ -7,15 +7,7 @@
 
 import UIKit
 
-struct Schedule: Codable {
-    var title: String
-    var date: Date
-    
-    init(title: String, date: Date) {
-        self.title = title
-        self.date = date
-    }
-}
+
 
 protocol AddTaskViewControllerOutput {
     func notifyClose()
@@ -25,14 +17,33 @@ class AddTaskViewController: UIViewController {
     
     @IBOutlet weak var taskNameTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var button: UIButton!
     
     let datePicker = UIDatePicker()
     
     var delegate: AddTaskViewControllerOutput?
+    var mode: Mode = .Regist
+    var selectedRow: Int?
+    var selectedSchedule: Schedule?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         createDatePicker()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if selectedRow != nil {
+            button.setTitle("編集", for: .normal)
+        }
+        if selectedSchedule != nil {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .medium
+            dateFormatter.locale = NSLocale(localeIdentifier: "ja_JP") as Locale?
+            dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+            taskNameTextField.text = selectedSchedule?.title
+            dateTextField.text = dateFormatter.string(from: selectedSchedule?.date ?? Date())
+        }
     }
     
     func createDatePicker(){
@@ -62,16 +73,19 @@ class AddTaskViewController: UIViewController {
     }
     
     @IBAction func didPushedRegistButton(_ sender: Any) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .medium
-        dateFormatter.locale = NSLocale(localeIdentifier: "ja_JP") as Locale?
-        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
-        let date = dateFormatter.date(from: dateTextField.text!)
+        guard let date = Utils.shared.covertStringToDate(type: .Date, date: dateTextField.text ?? "") else { return }
         var schedules = Utils.shared.load()
-        let schedule = Schedule.init(title: taskNameTextField.text ?? "", date: date ?? Date())
-        schedules?.append(schedule)
-        Utils.shared.save(Schedules: schedules ?? [])
+        
+        if mode == .Edit && selectedRow != nil {
+            var scheduleList: [Schedule] = []
+            scheduleList = Utils.shared.searchByDate(date: date)
+            let scheduleDate = Schedule.init(title: taskNameTextField.text ?? "", date: date)
+            scheduleList[selectedRow!] = scheduleDate
+        } else {
+            let schedule = Schedule.init(title: taskNameTextField.text ?? "", date: date)
+            schedules.append(schedule)
+        }
+        Utils.shared.save(Schedules: schedules)
         delegate?.notifyClose()
     }
 }
